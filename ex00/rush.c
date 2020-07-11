fb95eebac54addf43069fb93484575bcd3afe25e
jlee <ing5751@gmail.com>
2020-07-11 22:37:04 +0900

    Modify rush.c

--- ex00/rush.c
+++ ex00/rush.c
@@ -1,21 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   rush2.c                                            :+:      :+:    :+:   */
+/*   rush.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jlee <marvin@42.fr>                        +#+  +:+       +#+        */
+/*   By: jlee <ing5751@gmail.com>                   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/11 17:23:56 by jlee              #+#    #+#             */
-/*   Updated: 2020/07/11 21:45:37 by jlee             ###   ########.fr       */
+/*   Created: 2020/07/11 21:59:23 by jlee              #+#    #+#             */
+/*   Updated: 2020/07/11 22:35:50 by jlee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
-int *g_grid[4];
 
-int		is_valid_row_view(int row, int **row_view, int inverse)
+int		is_valid_row_view(int **grid, int row, int **row_view, int inverse)
 {
 	int i;
 	int k;
@@ -30,12 +29,12 @@ int		is_valid_row_view(int row, int **row_view, int inverse)
 		k = i;
 		if (inverse)
 			k = 3 - i;
-		if (g_grid[row][k] == 0)
+		if (grid[row][k] == 0)
 			return 1;
 
-	if (g_grid[row][k] > current_height)
+	if (grid[row][k] > current_height)
 	{
-		current_height = g_grid[row][k];
+		current_height = grid[row][k];
 		count++;
 	}
 	i++;
@@ -46,7 +45,7 @@ int		is_valid_row_view(int row, int **row_view, int inverse)
 	return 0;
 }
 
-int		is_valid_col_view(int col, int **col_view, int inverse)
+int		is_valid_col_view(int **grid, int col, int **col_view, int inverse)
 {
 	int i;
 	int k;
@@ -61,12 +60,12 @@ int		is_valid_col_view(int col, int **col_view, int inverse)
 		k = i;
 		if (inverse)
 			k = 3 - i;
-		if (g_grid[k][col] == 0)
+		if (grid[k][col] == 0)
 			return 1;
 
-	if (g_grid[k][col] > current_height)
+	if (grid[k][col] > current_height)
 	{
-		current_height = g_grid[k][col];
+		current_height = grid[k][col];
 		count++;
 	}
 	i++;
@@ -77,18 +76,18 @@ int		is_valid_col_view(int col, int **col_view, int inverse)
 	return 0;
 }
 
-int		is_valid_view(int row, int col, int **row_view, int **col_view)
+int		is_valid_view(int **grid, int row, int col, int **row_view, int **col_view)
 {
 	int is_valid;
 
-	is_valid = is_valid_row_view(row, row_view, 0)
-		&& is_valid_row_view(row, row_view, 1)
-		&& is_valid_col_view(col, col_view, 0)
-		&& is_valid_col_view(col, col_view, 1);
+	is_valid = is_valid_row_view(grid, row, row_view, 0)
+		&& is_valid_row_view(grid, row, row_view, 1)
+		&& is_valid_col_view(grid, col, col_view, 0)
+		&& is_valid_col_view(grid, col, col_view, 1);
 	return (is_valid);
 }
 
-int		is_unassigned(int *row, int *col)
+int		is_unassigned(int **grid, int *row, int *col)
 {
 	int i;
 	int j;
@@ -99,7 +98,7 @@ int		is_unassigned(int *row, int *col)
 		j = 0;
 		while (j < 4)
 		{
-			if(g_grid[i][j] == 0)
+			if(grid[i][j] == 0)
 			{
 				*row = i;
 				*col = j;
@@ -112,49 +111,49 @@ int		is_unassigned(int *row, int *col)
 	return 0;
 }
 
-int		is_valid_row_col(int row, int col)
+int		is_valid_row_col(int **grid, int row, int col)
 {
 	int i;
 
 	i = 0;
 	while (i < 4)
 	{
-		if (i != col && g_grid[row][i] == g_grid[row][col])
+		if (i != col && grid[row][i] == grid[row][col])
 			return (0);
-		if (i != row && g_grid[i][col] == g_grid[row][col])
+		if (i != row && grid[i][col] == grid[row][col])
 			return (0);
 		i++;
 	}
 	return (1);
 }
 
-int		process(int **row_view, int **col_view)
+int		process(int **grid, int **row_view, int **col_view)
 {
 	int val;
 	int row;
 	int col;
 
 	val = 1;
-	if (!is_unassigned(&row, &col))
+	if (!is_unassigned(grid, &row, &col))
 		return 1;
 	while (val <= 4)
 	{
-		g_grid[row][col] = val;
-		if (is_valid_row_col(row, col))
+		grid[row][col] = val;
+		if (is_valid_row_col(grid, row, col))
 		{
-			if (is_valid_view(row, col, row_view, col_view))
+			if (is_valid_view(grid, row, col, row_view, col_view))
 			{
-				if (process(row_view, col_view))
+				if (process(grid, row_view, col_view))
 					return 1;
 			}
 		}
-		g_grid[row][col] = 0;
+		grid[row][col] = 0;
 		val++;
 	}
 	return 0;
 }
 
-void		initialize_g_grid()
+void		initialize_grid(int **grid)
 {
 	int i;
     int j;
@@ -164,14 +163,14 @@ void		initialize_g_grid()
         j = 0;
         while (j < 4)
         {
-            g_grid[i][j] = 0;
+            grid[i][j] = 0;
             j++;
         }
         i++;
     }
 }
 
-void    print_g_grid()
+void    print_grid(int **grid)
 {
     char    g[4][4];
     int     i;
@@ -182,7 +181,7 @@ void    print_g_grid()
         j = 0;
         while (j < 4)
         {
-            g[i][j] = '0' + g_grid[i][j];
+            g[i][j] = '0' + grid[i][j];
             write(1, &g[i][j], 1);
 			if (j < 3)
 				write(1, " ", 1);
@@ -193,50 +192,64 @@ void    print_g_grid()
     }
 }
 
-int		main()
+int		main(int argc, char *argv[])
 {
 	int i;
-
-	i = 0;
+	int *grid[4];
 	int** row_view;
 	int** col_view;
 
+	if (argc != 2)
+		return -1;
+	
+	i = 0;
+	while (i < 4)
+	{
+		grid[i] = (int*)malloc(sizeof(int) * 4);
+		i++;
+	}
+	i = 0;
 	row_view = (int**)malloc(sizeof(int*) * 4);
 	col_view = (int**)malloc(sizeof(int*) * 4);
-	
 	while (i < 4)
 	{
 		row_view[i] = (int*)malloc(sizeof(int) * 2);
 		col_view[i] = (int*)malloc(sizeof(int) * 2);
 		i++;
 	}
-	
+		
+	col_view[0][0] = argv[1][0] - '0';
+	col_view[1][0] = argv[1][1] - '0';
+	col_view[2][0] = argv[1][2] - '0';
+	col_view[3][0] = argv[1][3] - '0';
+	col_view[0][1] = argv[1][4] - '0';
+	col_view[1][1] = argv[1][5] - '0';
+	col_view[2][1] = argv[1][6] - '0';
+	col_view[3][1] = argv[1][7] - '0';
+	row_view[0][0] = argv[1][8] - '0';
+	row_view[1][0] = argv[1][9] - '0';
+	row_view[2][0] = argv[1][10] - '0';
+	row_view[3][0] = argv[1][11] - '0';
+	row_view[0][1] = argv[1][12] - '0';
+	row_view[1][1] = argv[1][13] - '0';
+	row_view[2][1] = argv[1][14] - '0';
+	row_view[3][1] = argv[1][15] - '0';
+
+	initialize_grid(grid);
+	process(grid, row_view, col_view);
+	print_grid(grid);
 	i = 0;
 	while (i < 4)
 	{
-		g_grid[i] = (int*)malloc(sizeof(int) * 4);
+		free(grid[i]);
 		i++;
 	}
-
-	col_view[0][0] = 2;
-	col_view[1][0] = 3;
-	col_view[2][0] = 2;
-	col_view[3][0] = 1;
-	col_view[0][1] = 3;
-	col_view[1][1] = 1;
-	col_view[2][1] = 2;
-	col_view[3][1] = 2;
-	row_view[0][0] = 3;
-	row_view[1][0] = 1;
-	row_view[2][0] = 2;
-	row_view[3][0] = 2;
-	row_view[0][1] = 1;
-	row_view[1][1] = 3;
-	row_view[2][1] = 2;
-	row_view[3][1] = 2;
-
-	initialize_g_grid();
-	process(row_view, col_view);
-	print_g_grid();
+	i = 0;
+	while (i < 4)
+	{
+		free(row_view[i]);
+		free(col_view[i]);
+	i++;
+	}
 	return 0;
 }
